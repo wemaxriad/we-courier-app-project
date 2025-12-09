4341f2d417ee7370a6451633df9645198780853f
riadolivine <devolivineriad@gmail.com>
2025-12-09 13:53:26 +0600

    update all app

--- wecourier_deliveryapp/android/app/src/main/kotlin/duncanghexpress/courier/com/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.shohoz.courier.delivery
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- wecourier_merchantapp/android/app/src/main/kotlin/com/example/merchant/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.shohozcourier.merchant
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
